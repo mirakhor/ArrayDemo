f06fb848007643ad3988eec8d384a72c755cd807
Elnaz Mirakhorli <mirakhor@sheridancollege.ca>
2023-05-19 13:33:25 -0400

    Revert "added comment again"
    This reverts commit 73b1b5ed7958ef62764c976a6c7875dc5f9569c0.

--- src/arraysdemo/ArraysDemo.java
+++ src/arraysdemo/ArraysDemo.java
@@ -16,7 +16,6 @@ public class ArraysDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //comment in this file added
         System.out.println("Enter a word");
         Scanner input = new Scanner(System.in);
         String myWord = input.nextLine();
