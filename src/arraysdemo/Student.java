0b7963fdac3fc7b3db40dc726839ac27490c0974
Elnaz Mirakhorli <mirakhor@sheridancollege.ca>
2023-05-19 14:21:48 -0400

    adding new attribute address

--- src/arraysdemo/Student.java
+++ src/arraysdemo/Student.java
@@ -10,10 +10,19 @@ public class Student {
     //hellowwww
     private String sId;
     private String sName;
+    private String address;
 
     public Student(String sId, String sName) {
         this.sId = sId;
         this.sName = sName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
     }
         
 
